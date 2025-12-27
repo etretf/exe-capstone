c52018e0350e4b8c93de4800558b0d088cffce4e
Emma Souannhaphanh <77692774+etretf@users.noreply.github.com>
2025-12-26 20:48:06 -0500

    Implemented speaking player's name between two audio clips

--- Assets/Scenes/Demos/NameTTS/TTSDemo.cs
+++ Assets/Scenes/Demos/NameTTS/TTSDemo.cs
@@ -1,26 +1,59 @@
-using Meta.WitAi.TTS.Utilities;
 using UnityEngine;
+using Meta.WitAi.TTS.Utilities;
+using System.Collections;
+using Meta.WitAi.TTS.Data;
+using System;
+using Meta.WitAi.TTS;
 
-public class TTSDemo : MonoBehaviour
+public class MetaVoiceSequencePlayer : MonoBehaviour
 {
+    public AudioClip clip1;
+    public AudioClip clip2;
+    public AudioClip fetchedClip;
+    bool ttsPlayed = false;
+    public string nameText = "Emma";
+
+    public AudioSource audioSource;
     public TTSSpeaker ttsSpeaker;
-    public string playerName;
 
+    void Awake()
+    {
+        ttsSpeaker.Events.OnPlaybackComplete.AddListener(onTTSPlayed);
+    }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void onTTSPlayed(TTSSpeaker arg0, TTSClipData arg1)
+    {
+        ttsPlayed = true;
+    }
+
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void speakNameBetweenPhrases(string name)
     {
-        
+        StartCoroutine(playAudioClips());
     }
 
-    public void onSpeak(string name)
+    IEnumerator playAudioClips()
     {
-        ttsSpeaker.Speak(name);
+        playAudioClip(clip1);
+        yield return new WaitForSeconds(clip1.length);
+
+        ttsSpeaker.Speak(nameText);
+        yield return new WaitForSeconds(1.5f);
+
+        playAudioClip(clip2);
+
+
+        ttsPlayed=false;
     }
+
+    void playAudioClip(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.loop = false;
+        audioSource.PlayOneShot(clip);
+    }
+
 }
