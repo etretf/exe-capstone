26e148a13f9e4e3d60283c6ab74f5a373a8e3bf7
Amina Al-Helali <101413675+acce8711@users.noreply.github.com>
2026-01-04 12:51:33 -0500

    added instructional text

---
+++ Assets/Scenes/Shared/Scripts/InstructionalText.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+
+public class InstructionalText : MonoBehaviour
+{
+    [SerializeField] private GameObject text;
+    [SerializeField] private GameObject text_canvas;
+
+    //methods to modify text properties through code
+    public void SetTextValue(string value)
+    {
+        text.GetComponent<TextMeshProUGUI>().text = value;
+    }
+    public void SetFontSize(int value)
+    {
+        text.GetComponent<TextMeshProUGUI>().fontSize = value;
+    }
+    public void SetTextPosition(Vector3 position)
+    {
+        gameObject.transform.position = position;
+    }
+
+    public void SetTextRoation(Quaternion rotation)
+    {
+        gameObject.transform.rotation = rotation;
+    }
+
+    public void Show()
+    {
+        text_canvas.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        text_canvas.SetActive(false);
+    }
+}
